1dee81f888b61e54f2d38e7c2f8d61ec97d5b7f6
William Rehwinkel <49931356+FiskFan1999@users.noreply.github.com>
2022-02-25 16:48:55 -0500

    Update constText.go
    modified wording

--- constText.go
+++ constText.go
@@ -1,9 +1,9 @@
 package main
 
 const (
-	SubmitPageText = "Thank you for your submission! If you feel comfortable submitting incipits, please test your incipits in the lilypond sandbox to ensure that they will look correctly. (Incipits are not required, so do not feel pressured to include them unless you understand the syntax and know what you are doing.)"
+	SubmitPageText = "Thank you for your submission! If you feel comfortable submitting Lilypond incipits, please test them in the lilypond sandbox to ensure that they will look correctly. (Please note that incipits are not required, so do not feel pressured to include them unless you understand the syntax and know what you are doing.)"
 
-	LilypondPageText = "Welcome to the Lilypond Sandbox! Please enter the incipit as if it were inside the \\score block of the lilypond input file (as demonstrated below). Use this page to ensure that your incipit looks as intended and does not take up too much space. As for the size, kindly keep your length of incipit to around one measure, or approximately ten to twelve notes in a row. Please note that incipits are optional, so do not feel pressured to enter them unless you know what you are doing. Consider the following examples for some inspiration for the length of the incipit."
+	LilypondPageText = "Welcome to the Lilypond Sandbox! Please enter the incipit as if it were injected into the \\score block of the lilypond input file (as demonstrated above). Use this page to ensure that your incipit looks as intended and does not take up too much space. As for the size, kindly keep your length of incipit to around one measure, or approximately ten to twelve notes in a row. Please note that incipits are optional, so do not feel pressured to enter them unless you know what you are doing. Consider the above examples for some inspiration for the length of the incipit."
 )
 
 var LilyIncipitExamples []string = []string{
